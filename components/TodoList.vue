56fe32d119691b25c65959fbd3cea454933f77e3
rubisanto <antoine.gogce@capgemini.com>
2023-09-28 14:37:14 +0200

    todoList uses array

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    <h1 class="center">TodoList en Vue.Js</h1>
-    <ul>
+  <div class="container">
+    <h1 class="text-center mt-5 mb-4">TodoList en Vue.Js</h1>
+    <ul class="list-group">
       <li
-      v-for="(todo, index) in todos"
-      :key="index" >
+        v-for="(todo, index) in todos"
+        :key="index"
+        class="list-group-item d-flex justify-content-between align-items-center">
         {{ todo.text }}
+        <button class="btn btn-danger cursor-pointer" @click="deleteTodo(index)">X</button>
       </li>
     </ul>
-
   </div>
 </template>
+
+
 
 <script lang="ts">
 
@@ -26,7 +29,9 @@ export default {
       }
   },
   methods:{
-
+    deleteTodo(index: number){
+      this.todos.splice(index, 1);
+    }
   }
 }
 
@@ -45,4 +50,6 @@ export default {
     list-style-type: none;
     margin-bottom: 5px;
   }
+
+
 </style>
